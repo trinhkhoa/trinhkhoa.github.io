b8a66210b6391ff2e2e793e4901cdd5d7a1293e9
brIan_trInh <77667121+trinhkhoa@users.noreply.github.com>
2022-12-31 19:56:35 -0500

    Update game.js

--- game.js
+++ game.js
@@ -2,17 +2,58 @@ var canvas = document.getElementById('game-canvas');
 var context = canvas.getContext('2d');
 
 function update() {
-  // Update game state
+  // Update player position
+  player.x += player.dx;
+  player.y += player.dy;
+
+  // Update enemy positions
+  enemies.forEach(function(enemy) {
+    enemy.x += enemy.dx;
+    enemy.y += enemy.dy;
+  });
+
+  // Check for collisions
+  enemies.forEach(function(enemy) {
+    if (collides(player, enemy)) {
+      // Handle collision
+    }
+  });
 }
 
+function collides(a, b) {
+  return (a.x < b.x + b.width &&
+          a.x + a.width > b.x &&
+          a.y < b.y + b.height &&
+          a.y + a.height > b.y);
+}
+
+
 function render() {
-  // Render game
+  // Clear canvas
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw player
+  context.fillStyle = player.color;
+  context.fillRect(player.x, player.y, player.width, player.height);
+
+  // Draw enemies
+  enemies.forEach(function(enemy) {
+    context.fillStyle = enemy.color;
+    context.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+  });
 }
 
 function gameLoop() {
   update();
   render();
-  requestAnimationFrame(gameLoop);
+
+  // Check if game is over
+  if (gameOver) {
+    // Display game over screen
+  } else {
+    // Continue running game loop
+    requestAnimationFrame(gameLoop);
+  }
 }
 
 gameLoop();
